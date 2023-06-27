3642f7df15728bbda74b5a6525d1e5df09a797be
Marianne Metais <metais.marianne@gmail.com>
2023-06-27 11:21:43 +0200

    cleanup

--- scripts/datasets.py
+++ scripts/datasets.py
@@ -1,9 +1,6 @@
-import argparse
 from pathlib import Path
 from dataclasses import dataclass
 from typing import Iterable, Optional, List
-import logging
-import tqdm
 import pandas as pd
 
 
@@ -11,9 +8,9 @@ import pandas as pd
 class NoiseSegment:
     """Class for a noise segment in AudioSet"""
     noise_id: str
-    labels: List[str]
     start: float
     end: float
+    labels: List[str]
 
     @property
     def duration(self) -> float:
@@ -22,37 +19,38 @@ class NoiseSegment:
     @property
     def filename(self) -> str:
         return f"{self.noise_id}_{int(self.start*1000)}_{int(self.end*1000)}.flac"
-    
-    @property
-    def label_names(self) -> List[str]:
-        pass
 
 
 @dataclass
 class AudioSet:
     """Class for AudioSet"""
     metadata_path: Path
-    label_path: Path = None
-    # metadata: pd.DataFrame = None
+    label_path: Optional[Path] = None
 
     def __post_init__(self):
-        self.metadata = pd.read_csv(
-            self.metadata_path,
-            sep=',\s+',
-            skiprows=3,
-            names=['file_id', 'start', 'end', 'labels'],
-            converters={'labels': eval},
-            engine='python'
-        )
-        self.metadata.labels = self.metadata.labels.str.split(",")
-
         if self.label_path is not None:
+            self.metadata = pd.read_csv(
+                self.metadata_path,
+                sep=',\s+',
+                skiprows=3,
+                names=['file_id', 'start', 'end', 'labels'],
+                converters={'labels': eval},
+                engine='python'
+            )
+            self.metadata.labels = self.metadata.labels.str.split(",")
+
             df = pd.read_csv(self.label_path)
             label_name = df.set_index('mid')['display_name'].to_dict()
             self.metadata.labels = self.metadata.labels.apply(label_name)
 
+        else:
+            self.metadata = pd.read_csv(
+                self.metadata_path,
+                index_col=0
+            )
+
     def __iter__(self) -> Iterable[NoiseSegment]:
-        for index, segment in tqdm(self.metadata.iterrows(), total=self.metadata.shape[0]):
+        for index, segment in self.metadata.iterrows():
             noise_id, start, end, labels = segment
             noise_segment = NoiseSegment(noise_id, start, end, labels)
             yield noise_segment
